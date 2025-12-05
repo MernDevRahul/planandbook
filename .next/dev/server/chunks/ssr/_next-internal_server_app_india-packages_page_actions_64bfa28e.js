8b20afeb6b6bd1c705d69b9d6db4834290433e52
MernDevRahul <tech.rahul.shrivastav@gmail.com>
2025-12-05 11:41:46 +0530

    Remove .next folder from tracking

--- .next/dev/server/chunks/ssr/_next-internal_server_app_india-packages_page_actions_64bfa28e.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/india-packages/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_india-packages_page_actions_64bfa28e.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"g3XXDtP0Dp0Ks0vpYzGDFVqAplcOZ6Yl3lzVxGQ9mbw=\"\n}"
